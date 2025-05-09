a2f8dc4976210dca92e712f4021102ab1726ffe0
Hessel <h.a.middendorp@icloud.com>
2025-05-09 14:22:34 +0200

    update

--- src/routes/account/profile/+page.svelte
+++ src/routes/account/profile/+page.svelte
@@ -28,7 +28,9 @@
 				required={$constraints.firstName?.required}
 			/>
 			{#if $errors.firstName}
-				<p>{$errors.firstName[0]}</p>
+				{#each $errors.firstName as errorMsg (errorMsg)}
+					<p>{errorMsg}</p>
+				{/each}
 			{/if}
 		</div>
 
@@ -44,7 +46,9 @@
 				required={$constraints.lastName?.required}
 			/>
 			{#if $errors.lastName}
-				<p>{$errors.lastName[0]}</p>
+				{#each $errors.lastName as errorMsg (errorMsg)}
+					<p>{errorMsg}</p>
+				{/each}
 			{/if}
 		</div>
 
@@ -59,106 +63,122 @@
 				aria-invalid={$errors.companyName ? 'true' : undefined}
 			/>
 			{#if $errors.companyName}
-				<p>{$errors.companyName[0]}</p>
+				{#each $errors.companyName as errorMsg (errorMsg)}
+					<p>{errorMsg}</p>
+				{/each}
 			{/if}
 		</div>
 
-		<h2>Billing Address</h2>
-		<p>If you’d like to add a postal address to every invoice, enter it here.</p>
+		<fieldset>
+			<legend>Billing Address</legend>
+			<p>If you’d like to add a postal address to every invoice, enter it here.</p>
 
-		<div>
-			<label for="streetAndNumber">Street and Number</label>
-			<input
-				type="text"
-				id="streetAndNumber"
-				name="streetAndNumber"
-				bind:value={$form.streetAndNumber}
-				disabled={$submitting}
-				aria-invalid={$errors.streetAndNumber ? 'true' : undefined}
-			/>
-			{#if $errors.streetAndNumber}
-				<p>{$errors.streetAndNumber[0]}</p>
-			{/if}
-		</div>
+			<div>
+				<label for="streetAndNumber">Street and Number</label>
+				<input
+					type="text"
+					id="streetAndNumber"
+					name="streetAndNumber"
+					bind:value={$form.streetAndNumber}
+					disabled={$submitting}
+					aria-invalid={$errors.streetAndNumber ? 'true' : undefined}
+				/>
+				{#if $errors.streetAndNumber}
+					{#each $errors.streetAndNumber as errorMsg (errorMsg)}
+						<p>{errorMsg}</p>
+					{/each}
+				{/if}
+			</div>
 
-		<div>
-			<label for="streetAdditional">Additional Address Line (e.g., Apt, Suite)</label>
-			<input
-				type="text"
-				id="streetAdditional"
-				name="streetAdditional"
-				bind:value={$form.streetAdditional}
-				disabled={$submitting}
-				aria-invalid={$errors.streetAdditional ? 'true' : undefined}
-			/>
-			{#if $errors.streetAdditional}
-				<p>{$errors.streetAdditional[0]}</p>
-			{/if}
-		</div>
+			<div>
+				<label for="streetAdditional">Additional Address Line (e.g., Apt, Suite)</label>
+				<input
+					type="text"
+					id="streetAdditional"
+					name="streetAdditional"
+					bind:value={$form.streetAdditional}
+					disabled={$submitting}
+					aria-invalid={$errors.streetAdditional ? 'true' : undefined}
+				/>
+				{#if $errors.streetAdditional}
+					{#each $errors.streetAdditional as errorMsg (errorMsg)}
+						<p>{errorMsg}</p>
+					{/each}
+				{/if}
+			</div>
 
-		<div>
-			<label for="postalCode">Postal Code</label>
-			<input
-				type="text"
-				id="postalCode"
-				name="postalCode"
-				bind:value={$form.postalCode}
-				disabled={$submitting}
-				aria-invalid={$errors.postalCode ? 'true' : undefined}
-			/>
-			{#if $errors.postalCode}
-				<p>{$errors.postalCode[0]}</p>
-			{/if}
-		</div>
+			<div>
+				<label for="postalCode">Postal Code</label>
+				<input
+					type="text"
+					id="postalCode"
+					name="postalCode"
+					bind:value={$form.postalCode}
+					disabled={$submitting}
+					aria-invalid={$errors.postalCode ? 'true' : undefined}
+				/>
+				{#if $errors.postalCode}
+					{#each $errors.postalCode as errorMsg (errorMsg)}
+						<p>{errorMsg}</p>
+					{/each}
+				{/if}
+			</div>
 
-		<div>
-			<label for="city">City</label>
-			<input
-				type="text"
-				id="city"
-				name="city"
-				bind:value={$form.city}
-				disabled={$submitting}
-				aria-invalid={$errors.city ? 'true' : undefined}
-			/>
-			{#if $errors.city}
-				<p>{$errors.city[0]}</p>
-			{/if}
-		</div>
+			<div>
+				<label for="city">City</label>
+				<input
+					type="text"
+					id="city"
+					name="city"
+					bind:value={$form.city}
+					disabled={$submitting}
+					aria-invalid={$errors.city ? 'true' : undefined}
+				/>
+				{#if $errors.city}
+					{#each $errors.city as errorMsg (errorMsg)}
+						<p>{errorMsg}</p>
+					{/each}
+				{/if}
+			</div>
 
-		<div>
-			<label for="region">Region/State/Province (Optional)</label>
-			<input
-				type="text"
-				id="region"
-				name="region"
-				bind:value={$form.region}
-				disabled={$submitting}
-				aria-invalid={$errors.region ? 'true' : undefined}
-			/>
-			{#if $errors.region}
-				<p>{$errors.region[0]}</p>
-			{/if}
-		</div>
+			<div>
+				<label for="region">Region/State/Province (Optional)</label>
+				<input
+					type="text"
+					id="region"
+					name="region"
+					bind:value={$form.region}
+					disabled={$submitting}
+					aria-invalid={$errors.region ? 'true' : undefined}
+				/>
+				{#if $errors.region}
+					{#each $errors.region as errorMsg (errorMsg)}
+						<p>{errorMsg}</p>
+					{/each}
+				{/if}
+			</div>
 
-		<div>
-			<label for="country">Country (2-letter ISO code, e.g., US, GB)</label>
-			<input
-				type="text"
-				id="country"
-				name="country"
-				bind:value={$form.country}
-				minlength="2"
-				maxlength="2"
-				title="Enter a 2-letter ISO country code"
-				disabled={$submitting}
-				aria-invalid={$errors.country ? 'true' : undefined}
-				style="text-transform: uppercase;"
-			/>
-			{#if $errors.country}
-				<p>{$errors.country[0]}</p>
-			{/if}
-		</div>
+			<div>
+				<label for="country">Country (2-letter ISO code, e.g., US, GB)</label>
+				<input
+					type="text"
+					id="country"
+					name="country"
+					bind:value={$form.country}
+					minlength="2"
+					maxlength="2"
+					title="Enter a 2-letter ISO country code"
+					disabled={$submitting}
+					aria-invalid={$errors.country ? 'true' : undefined}
+					style="text-transform: uppercase;"
+				/>
+				{#if $errors.country}
+					{#each $errors.country as errorMsg (errorMsg)}
+						<p>{errorMsg}</p>
+					{/each}
+				{/if}
+			</div>
+		</fieldset>
 
 		{#if $message}
 			<p>{$message}</p>
