64eb1790fb0f0a7ddf0da133019846abd8428d10
Hessel <h.a.middendorp@icloud.com>
2025-05-14 18:25:45 +0200

    feat: add input styles

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -12,18 +12,24 @@
 
 <div>
 	<h1>Login</h1>
-	<form method="POST" use:enhance>
+	<form method="POST" class="flex flex-col gap-y-5" use:enhance>
 		<div>
-			<label for="email">Email:</label>
+			<label for="email" class="sr-only">Email:</label>
 			<input
 				type="email"
-				id="email"
 				name="email"
+				id="email"
 				bind:value={$form.email}
+				placeholder="Email"
 				required
 				disabled={$submitting}
 				aria-invalid={$errors.email ? 'true' : undefined}
-				placeholder="your@email.com"
+				class={[
+					'font-evolventa bg-foundation-white text-black-sheep placeholder:text-black-sheep w-full rounded-full px-[38px] py-[20px] text-base/[21px]',
+					'sm:text-lg/[24px]',
+					'md:text-xl/[27px]',
+					'lg:text-2xl/[32px]'
+				]}
 			/>
 			{#if $errors.email}
 				<p>{$errors.email[0]}</p>
@@ -38,7 +44,7 @@
 			type="submit"
 			disabled={$submitting}
 			class={[
-				'font-evolventa border-black-sheep text-black-sheep cursor-pointer rounded-full border px-[37px] py-[19px] text-base/[21px] shadow-[0_4px_4px_rgba(0,0,0,0.25)] hover:border-[5px] hover:px-[33px] hover:py-[15px] disabled:cursor-not-allowed disabled:border disabled:bg-[#ececec] disabled:px-[37px] disabled:py-[19px]',
+				'font-evolventa border-black-sheep text-black-sheep cursor-pointer self-start rounded-full border px-[37px] py-[19px] text-base/[21px] shadow-[0_4px_4px_rgba(0,0,0,0.25)] hover:border-[5px] hover:px-[33px] hover:py-[15px] disabled:cursor-not-allowed disabled:border disabled:bg-[#ececec] disabled:px-[37px] disabled:py-[19px]',
 				'sm:text-lg/6'
 			]}
 		>
