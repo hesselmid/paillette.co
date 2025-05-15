dea0662d58fb2e466fb7e666ecb3493b38e69e6b
Hessel <h.a.middendorp@icloud.com>
2025-05-15 11:35:04 +0200

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,15 @@
 	<title>Paillette.co</title>
 </svelte:head>
 
-<h1>Paillette</h1>
+<div class="bg-white py-[60px]">
+	<div class="container mx-auto">
+		<enhanced:img
+			src="$lib/assets/HomePage_drawing1 1.png"
+			alt=""
+			class={['h-auto w-[358px]', 'sm:w-[448px]', 'md:w-[498px]', 'xl:w-[616px]', '2xl:w-[685px]']}
+		/>
+		<h1 class="font-cormorant text-black-sheep text-center text-4xl/[44px] font-light">
+			“add a little shine to your story”
+		</h1>
+	</div>
+</div>
