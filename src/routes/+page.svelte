86dd4b596fc679ccc6b610bff329eef2e84c68e9
Hessel <h.a.middendorp@icloud.com>
2025-05-15 11:58:13 +0200

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,8 +5,9 @@
 <div class="bg-white py-[60px]">
 	<div class="container mx-auto">
 		<enhanced:img
-			src="$lib/assets/HomePage_drawing1 1.png"
+			src="$lib/assets/HomePage_drawing1 1.png?w=358;448;498;616;685;716;896;996;1232;1370"
 			alt=""
+			sizes="(min-width: 1536px) 685px, (min-width: 1280px) 616px, (min-width: 768px) 498px, (min-width: 640px) 448px, 358px"
 			class={['h-auto w-[358px]', 'sm:w-[448px]', 'md:w-[498px]', 'xl:w-[616px]', '2xl:w-[685px]']}
 		/>
 		<h1 class="font-cormorant text-black-sheep text-center text-4xl/[44px] font-light">
