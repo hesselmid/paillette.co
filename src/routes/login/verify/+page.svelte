4801f62d12e20197c61acf00ac7254305fc87b17
Hessel <h.a.middendorp@icloud.com>
2025-05-15 14:02:57 +0200

    feat: style verify route

--- src/routes/login/verify/+page.svelte
+++ src/routes/login/verify/+page.svelte
@@ -6,8 +6,21 @@
 	const { form, enhance, errors, message, submitting } = superForm(data.form);
 </script>
 
-<div>
-	<h1>Verification</h1>
+<svelte:head>
+	<title>Verify - Paillette.co</title>
+</svelte:head>
+
+<div class={['bg-white py-16', 'lg:py-20']}>
+	<h1
+		class={[
+			'font-apfel-grotezk-brukt text-black-sheep text-center text-3xl/[38px]',
+			'sm:text-4xl/[46px]',
+			'md:text-5xl/[62px]',
+			'lg:text-6xl/[77px]'
+		]}
+	>
+		Verification
+	</h1>
 	{#if data.email}
 		<p>
 			If you have an account, we have sent a code to <strong>{data.email}</strong>. Enter it below.
@@ -16,14 +29,19 @@
 		<p>Loading email information or session expired. <a href="/login">Try logging in again.</a></p>
 	{/if}
 
-	<form method="POST" use:enhance>
+	<form
+		method="POST"
+		class={['mx-auto mt-16 flex max-w-[324px] flex-col gap-y-5', 'sm:max-w-[490px]', 'lg:mt-20']}
+		use:enhance
+	>
 		<div>
-			<label for="otp">OTP:</label>
+			<label for="otp" class="sr-only">OTP:</label>
 			<input
 				type="text"
 				id="otp"
 				name="otp"
 				bind:value={$form.otp}
+				placeholder="012345"
 				pattern="\d*"
 				minlength="6"
 				maxlength="6"
@@ -32,6 +50,12 @@
 				required
 				disabled={$submitting || !data.email}
 				aria-invalid={$errors.otp ? 'true' : undefined}
+				class={[
+					'font-evolventa bg-foundation-white text-black-sheep placeholder:text-black-sheep w-full rounded-full px-[38px] py-[20px] text-base/[21px]',
+					'sm:text-lg/[24px]',
+					'md:text-xl/[27px]',
+					'lg:text-2xl/[32px]'
+				]}
 			/>
 			{#if $errors.otp}
 				<p>{$errors.otp[0]}</p>
