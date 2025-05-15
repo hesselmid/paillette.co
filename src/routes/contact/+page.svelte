4bafb7c27fdc9cad5bda615fdb86a1b4c12ea2a4
Hessel <h.a.middendorp@icloud.com>
2025-05-15 14:23:15 +0200

    update

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -2,4 +2,21 @@
 	<title>Contact - Paillette.co</title>
 </svelte:head>
 
-<h1>Contact</h1>
+<section class={['bg-white py-16', 'xl:py-20']}>
+	<div class="container mx-auto">
+		<div class={['flex flex-col items-center', 'lg:flex-row']}>
+			<h1
+				class={[
+					'font-cormorant text-black-sheep text-center text-3xl/[36px] font-light italic',
+					'sm:text-left sm:text-4xl/[44px]',
+					'md:text-5xl/[58px]',
+					'xl:text-6xl/[73px]',
+					'2xl:text-7xl/[87px]'
+				]}
+			>
+				“Inspired by what you see?<br /> Let’s start a conversation and explore how our prints can tell
+				your brand’s unique story.”
+			</h1>
+		</div>
+	</div>
+</section>
