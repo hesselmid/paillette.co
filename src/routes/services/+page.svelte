c4574b3be25030f0dc83fde297ae4b0b22c00e59
Hessel <h.a.middendorp@icloud.com>
2025-05-20 14:48:24 +0200

    update

--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import PailletteIcon from './PailletteIcon.svelte';
 
-	let open = $state(false);
+	let open = $state(true);
 </script>
 
 <svelte:head>
@@ -49,7 +49,14 @@
 		<div
 			class={[
 				'absolute size-[42px]',
-				open ? 'top-[576px] left-[15px]' : 'top-[597px] left-[153px]'
+				'sm:size-[66px]',
+				'md:size-[84px]',
+				open ? 'top-[576px] left-[15px]' : 'top-[597px] left-[153px]',
+				open ? 'sm:top-[623px] sm:left-[96px]' : 'sm:top-[654px] sm:left-[254px]',
+				open ? 'md:top-[565px] md:left-[83px]' : 'md:top-[606px] md:left-[300px]',
+				open ? 'lg:top-[] lg:left-[]' : 'lg:top-[] lg:left-[]',
+				open ? 'xl:top-[] xl:left-[]' : 'xl:top-[] xl:left-[]',
+				open ? '2xl:top-[] 2xl:left-[]' : '2xl:top-[] 2xl:left-[]'
 			]}
 		>
 			<PailletteIcon primaryColor="#D4EBF8" secondaryColor="#EEF7FC" />
@@ -59,7 +66,14 @@
 		<div
 			class={[
 				'absolute size-[42px]',
-				open ? 'top-[640px] left-[61px]' : 'top-[633.4px] left-[132px]'
+				'sm:size-[66px]',
+				'md:size-[84px]',
+				open ? 'top-[640px] left-[61px]' : 'top-[633.4px] left-[132px]',
+				open ? 'sm:top-[735px] sm:left-[152px]' : 'sm:top-[711.2px] sm:left-[221px]',
+				open ? 'md:top-[718px] md:left-[32px]' : 'md:top-[678.83px] md:left-[258px]',
+				open ? 'lg:top-[] lg:left-[]' : 'lg:top-[] lg:left-[]',
+				open ? 'xl:top-[] xl:left-[]' : 'xl:top-[] xl:left-[]',
+				open ? '2xl:top-[] 2xl:left-[]' : '2xl:top-[] 2xl:left-[]'
 			]}
 		>
 			<PailletteIcon primaryColor="#ff5249" secondaryColor="#ffbab6" />
@@ -69,7 +83,14 @@
 		<div
 			class={[
 				'absolute size-[42px]',
-				open ? 'top-[705px] left-[139px]' : 'top-[669.75px] left-[153px]'
+				'sm:size-[66px]',
+				'md:size-[84px]',
+				open ? 'top-[705px] left-[139px]' : 'top-[669.75px] left-[153px]',
+				open ? 'sm:top-[807px] sm:left-[230px]' : 'sm:top-[768.32px] sm:left-[254px]',
+				open ? 'md:top-[796px] md:left-[148px]' : 'md:top-[751.5px] md:left-[300px]',
+				open ? 'lg:top-[] lg:left-[]' : 'lg:top-[] lg:left-[]',
+				open ? 'xl:top-[] xl:left-[]' : 'xl:top-[] xl:left-[]',
+				open ? '2xl:top-[] 2xl:left-[]' : '2xl:top-[] 2xl:left-[]'
 			]}
 		>
 			<PailletteIcon primaryColor="#541212" secondaryColor="#BBA0A0" />
@@ -79,7 +100,14 @@
 		<div
 			class={[
 				'absolute size-[42px]',
-				open ? 'top-[633px] left-[174px]' : 'top-[633.4px] left-[174px]'
+				'sm:size-[66px]',
+				'md:size-[84px]',
+				open ? 'top-[633px] left-[174px]' : 'top-[633.4px] left-[174px]',
+				open ? 'sm:top-[711px] sm:left-[287px]' : 'sm:top-[711.2px] sm:left-[287px]',
+				open ? 'md:top-[679px] md:left-[342px]' : 'md:top-[678.83px] md:left-[342.04px]',
+				open ? 'lg:top-[] lg:left-[]' : 'lg:top-[] lg:left-[]',
+				open ? 'xl:top-[] xl:left-[]' : 'xl:top-[] xl:left-[]',
+				open ? '2xl:top-[] 2xl:left-[]' : '2xl:top-[] 2xl:left-[]'
 			]}
 		>
 			<PailletteIcon primaryColor="#CDC526" secondaryColor="#EBE8A8" />
@@ -89,7 +117,14 @@
 		<div
 			class={[
 				'absolute size-[42px]',
-				open ? 'top-[576px] left-[258px]' : 'top-[597.03px] left-[195px]'
+				'sm:size-[66px]',
+				'md:size-[84px]',
+				open ? 'top-[576px] left-[258px]' : 'top-[597.03px] left-[195px]',
+				open ? 'sm:top-[623px] sm:left-[350px]' : 'sm:top-[654.05px] sm:left-[320px]',
+				open ? 'md:top-[578px] md:left-[429px]' : 'md:top-[606.06px] md:left-[384.05px]',
+				open ? 'lg:top-[] lg:left-[]' : 'lg:top-[] lg:left-[]',
+				open ? 'xl:top-[] xl:left-[]' : 'xl:top-[] xl:left-[]',
+				open ? '2xl:top-[] 2xl:left-[]' : '2xl:top-[] 2xl:left-[]'
 			]}
 		>
 			<PailletteIcon primaryColor="#06D001" secondaryColor="#9BEC99" />
@@ -99,7 +134,14 @@
 		<div
 			class={[
 				'absolute size-[42px]',
-				open ? 'top-[705px] left-[315px]' : 'top-[669.75px] left-[195px]'
+				'sm:size-[66px]',
+				'md:size-[84px]',
+				open ? 'top-[705px] left-[315px]' : 'top-[669.75px] left-[195px]',
+				open ? 'sm:top-[794px] sm:left-[407px]' : 'sm:top-[768.32px] sm:left-[320px]',
+				open ? 'md:top-[796px] md:left-[517px]' : 'md:top-[751.56px] md:left-[384.05px]',
+				open ? 'lg:top-[] lg:left-[]' : 'lg:top-[] lg:left-[]',
+				open ? 'xl:top-[] xl:left-[]' : 'xl:top-[] xl:left-[]',
+				open ? '2xl:top-[] 2xl:left-[]' : '2xl:top-[] 2xl:left-[]'
 			]}
 		>
 			<PailletteIcon primaryColor="#EE66A6" secondaryColor="#F8C2DB" />
@@ -109,7 +151,14 @@
 		<div
 			class={[
 				'absolute size-[42px]',
-				open ? 'top-[616px] left-[331px]' : 'top-[633.4px] left-[216px]'
+				'sm:size-[66px]',
+				'md:size-[84px]',
+				open ? 'top-[616px] left-[331px]' : 'top-[633.4px] left-[216px]',
+				open ? 'sm:top-[698px] sm:left-[478px]' : 'sm:top-[711.2px] sm:left-[353px]',
+				open ? 'md:top-[667px] md:left-[601px]' : 'md:top-[678.83px] md:left-[426.07px]',
+				open ? 'lg:top-[] lg:left-[]' : 'lg:top-[] lg:left-[]',
+				open ? 'xl:top-[] xl:left-[]' : 'xl:top-[] xl:left-[]',
+				open ? '2xl:top-[] 2xl:left-[]' : '2xl:top-[] 2xl:left-[]'
 			]}
 		>
 			<PailletteIcon primaryColor="#3D3BF3" secondaryColor="#D5D4FF" />
