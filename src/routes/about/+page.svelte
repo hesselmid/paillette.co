da673ebaf1dfbc1c599aa829eb8bbdbc552ef17f
Hessel <h.a.middendorp@icloud.com>
2025-06-23 11:37:41 +0200

    feat: some scrollbar shenanigans

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -38,3 +38,63 @@
 		/>
 	</div>
 </div>
+
+<section class="bg-enoki py-9">
+	<div class="container mx-auto">
+		<h2
+			class={[
+				'font-cormorant text-black-sheep text-center text-3xl/[36px] font-light',
+				'sm:text-4xl/[44px]',
+				'md:text-5xl/[58px]'
+			]}
+		>
+			About us:
+		</h2>
+		<div
+			class={[
+				'font-cormorant text-black-sheep mx-4 mt-9 h-[456px] space-y-[19px] overflow-y-scroll pr-4 text-base/[19px] [&::-webkit-scrollbar]:w-6 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-[#541212] [&::-webkit-scrollbar-track]:rounded-full [&::-webkit-scrollbar-track]:border [&::-webkit-scrollbar-track]:border-[#541212] [&::-webkit-scrollbar-track]:bg-[#bba0a0]',
+				'sm:h-[331px] sm:space-y-[24px] sm:text-xl/[24px]',
+				'md:mr-8 md:h-[580px] md:space-y-[29px] md:pr-[30px] md:text-2xl/[29px]'
+			]}
+		>
+			<p>
+				In an industry where fresh perspectives and quality craftsmanship can set a brand apart,
+				Paillette offers a unique source of exclusive, one-of-a-kind prints created by rising design
+				talent. After years running my own design studio as a textile developer and Head of design
+				at a textile company for both high street and high-end brands, I founded Paillette to
+				address a gap I often saw: access to original, thoughtful design that connects brand
+				identity with impactful storytelling. I wanted to create a space that brings together
+				ambitious designers and visionary brands, empowering both to elevate their craft.
+			</p>
+			<p>
+				My journey in fashion has taken me from presenting collections at international fashion
+				events to collaborating with brands at every level. In my last role, I led the creation of
+				large-scale textile collections that blended print, embroidery, and specialty textiles,
+				developing hundreds of designs each season for renowned brands. With a background in diverse
+				textile techniques, from high-end knits to circular print design, I’ve built a deep
+				knowledge of what resonates in the market and how quality, detail, and originality drive a
+				brand’s story forward.
+			</p>
+			<p>
+				At Paillette, we’re passionate about offering more than just beautiful textiles. Each design
+				represents a fresh voice, a commitment to sustainability, and a collaboration built on
+				purpose. For brands, Paillette is a source of prints crafted with the highest attention to
+				detail, quality, and originality. By working with emerging designers on our platform, brands
+				gain access to exclusive prints that set them apart, creating products that embody
+				authenticity and innovation.
+			</p>
+			<p>
+				Through Paillette, I aim to create a new standard in the industry, fostering a connection
+				between brands and designers that celebrates responsible production, creative freedom, and
+				meaningful impact. We’re building a space where brands can discover unique textiles with
+				stories worth sharing, and where designers are supported to bring their craft to life in
+				ways that inspire and elevate.
+			</p>
+			<p>
+				Our mission is simple: to connect, create, and inspire a more thoughtful industry. By
+				partnering with Paillette, you’re joining a movement toward a fashion future that values
+				originality, inclusivity, and enduring quality.
+			</p>
+		</div>
+	</div>
+</section>
